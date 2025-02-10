7bb5016393c5d7fb5e6c35fcfaf58775257974fc
Charles-Ivan Dury <charlesivan.dury@gmx.fr>
2025-02-10 16:22:17 +0100

    sent files

---
+++ snake.js
@@ -0,0 +1,15 @@
+
+function rndPos() {
+    // 324
+    return(Math.floor(Math.random() * 324));
+}
+
+function start() {
+    console.log("Starting game");
+
+    
+}
+
+window.addEventListener("load", (ev)=>{
+
+});
